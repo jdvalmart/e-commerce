e6d311a5bd8aeed4f356943135ddfa3c1c7094d1
jdvalmart <juanvalencia9411@outlook.com>
2024-07-23 21:54:38 -0500

    realizo las rutas con React router y Browserrouter, se actualiza el README

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,23 +1,30 @@
-import { Home } from '../Home'
+import {useRoutes,BrowserRouter} from 'react-router-dom'
+import {Home} from '../Home'
 import {MyAccount} from '../MyAccount'
 import {MyOrder} from '../MyOrder'
 import {MyOrders} from '../MyOrders'
 import {NotFound} from '../NotFound'
 import {SignIn} from '../SignIn'
-import '../App.css'
+import './App.css'
 
-function App() {
+const AppRoutes = () => {
+  
+  let routes = useRoutes([
+    {path:'/',element: <Home/>},
+    {path:'/MyAccount',element: <MyAccount/>},
+    {path:'/MyOrder',element: <MyOrder/>},
+    {path:'/MyOrders',element: <MyOrders/>},
+    {path:'/*',element: <NotFound/>},
+    {path:'/SignIn',element: <SignIn/>}
+  ])
+  return routes
+}
 
+const App = () => {
   return (
-   <div className='bg-red-400'>
-    <Home/>
-    <MyAccount/>
-    <MyOrder/>
-    <MyOrders/>
-    <NotFound/>
-    <SignIn/>
-
-    </div>
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
   )
 }
 
