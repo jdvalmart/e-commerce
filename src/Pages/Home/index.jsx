227fad4c64aa89dcd8f67cc767437bbc5a0c673d
jdvalmart <juanvalencia9411@outlook.com>
2024-07-24 21:39:56 -0500

    empezamos a usar los estados useState y useEfect para consumir una API en el e-commerce

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,31 @@
+import { useState, useEffect } from "react"
 import { Layout} from "../../components/Layout"
 import { Card } from "../../components/Card"
 
 
+
 function Home() {
+ const [ items, setItems ] = useState(null)
+
+ useEffect(()=> {
+    fetch('https://fakestoreapi.com/products')
+    .then( res =>res.json() )
+    .then( data => setItems(data))
+ },[])
+
 
   return (
    <Layout>
       home
-      <Card/>
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {
+          items?.map( item => (
+          <Card key={item.id} data={item} />
+        ))
+        }
+      
+      </div>
+     
    </Layout>
   )
 }
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,17 +1,17 @@
 
 
-function Card() {
+function Card({data}) {
     return (
         <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
             <figure className='relative mb-2 w-full h-4/5'>
-                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>Electronics </span>
-                <img className='w-full h-full object-cover rounded-lg ' src='https://images.pexels.com/photos/4887161/pexels-photo-4887161.jpeg' alt="headphone" />
+                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.category} </span>
+                <img className='w-full h-full object-cover rounded-lg ' src={data.image} alt={data.description} />
                 <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'>
                     +
                 </div>
                 <p className=' flex justify-between'>
-                    <span className='text-sm font-light'>headphone</span>
-                    <span className='text-lg font-medium'>$3000</span>
+                    <span className='text-sm font-light'>{data.title}</span>
+                    <span className='text-lg font-medium'>${data.price}</span>
                 </p>
             </figure>
         </div>
